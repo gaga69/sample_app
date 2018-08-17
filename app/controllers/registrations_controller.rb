d0f5d056b3b0b83c39e86c13dc9242946ba2841e
gaga69 <yumm.yumm.08feb@gmail.com>
2018-08-17 22:01:22 +0900

    Finish sign in

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,16 @@ class RegistrationsController < Devise::RegistrationsController
     def after_sign_up_path_for(resource)
         user_path(resource)
     end
+    
+    def create
+      @user = User.new(sign_up_params)
+      if @user.save
+        flash[:success] = "Welcom to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end
+    end
 
     private
 
