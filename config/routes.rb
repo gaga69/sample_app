7ab9d9c45b87436ce43a7fef60a6f50050ca2ecd
gaga69 <yumm.yumm.08feb@gmail.com>
2018-08-23 21:15:29 +0900

    Finish user destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => "registrations"
   }
-  resources :users, only: [:show, :index]
+  resources :users, only: [:show, :index, :destroy]
   root  'static_pages#home'
   get 'help', to: 'static_pages#help', as: :help
   get 'about',   to: 'static_pages#about',   as: :about
